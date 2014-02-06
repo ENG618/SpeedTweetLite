ec40f8ccff4d0520c9bec9f5930d737f99962243
eng618 <eng618@fullsail.edu>
2014-02-06 01:05:30 -0500

    Initial Commit

---
+++ SpeedTweetLite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpeedTweetLite
+//
+//  Created by Eric Garcia on 2/6/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
